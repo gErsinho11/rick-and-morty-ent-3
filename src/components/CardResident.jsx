a81857001d76560dc5f6eada27010e946fb3af7e
gErsinho11 <ingeniero.gersinho@gmail.com>
2023-11-28 21:40:39 -0500

    entregable-3

---
+++ src/components/CardResident.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react"
+import useFetch from "../hooks/useFetch"
+import "./styles/CardResident.css"
+
+const CardResident = ({ url }) => {
+
+  const [resident, getResident] = useFetch(url)
+
+  useEffect(() => {
+    getResident()
+  }, [])
+
+  console.log(resident)
+
+  return (
+    <article className="resident">
+      <header className="resident__header">
+        <img src={resident?.image} alt="" className="resident__image"/>
+        <div className="resident__status">
+          <div className={`resident__status-circle ${resident?.status}`}></div>
+          <span className="resident__status-value">{resident?.status}</span>
+        </div>
+        <section className="resident__body">
+          <h3 className="resident__name">{resident?.name}</h3>
+          <hr className="resident__separator" />
+          <ul className="resident__stats">
+            <li className="resident__stat__item"><span className="resident__stat__label">Specie</span><span className="resident__stat__value">{resident?.species}</span></li>
+            <li className="resident__stat__item"><span className="resident__stat__label">Origin</span><span className="resident__stat__value">{resident?.origin.name}</span></li>
+            <li className="resident__stat__item"><span className="resident__stat__label">Eppisodes where appear</span><span className="resident__stat__value">{resident?.episode.length}</span></li>
+          </ul>
+        </section>
+      </header>
+    </article>
+  )
+}
+
+export default CardResident
